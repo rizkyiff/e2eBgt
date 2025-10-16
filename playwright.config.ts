0651ceab2af910e551c0f70e9229f2f317c501c0
Moeh Roji Rizky Darmawan <m.r.rizky.d@gmail.com>
2025-10-16 23:09:05 +0700

    ci(pages): setup Pages, generate Allure, upload Playwright HTML

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -70,15 +69,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-  reporter: [
-    ["line"],
-    [
-      "allure-playwright",
-      {
-        resultsDir: "allure-results",
-      },
-    ],
-  ],
+  reporter: [["html"], ["line"], ["allure-playwright", { resultsDir: "allure-results" }]],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
